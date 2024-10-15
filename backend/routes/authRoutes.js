deaa0834c1ab4eecc92a89b10159fee6df016791
Batchuluun0605 <batchuluun@gmail.com>
2024-10-15 12:57:03 +0800

    back complete

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,9 +3,8 @@ import { getMe, login, logout, signup } from "../controller/auth.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 const router = express.Router();
 
-router.post("/signup", signup);
 router.get("/me", protectRoute, getMe);
-
+router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+
 import connectMongoDb from "./db/connectMongoDb.js";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
+cloudinary.config({
+  cloud_name: process.envCLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 connectMongoDb();
 const app = express();
@@ -16,6 +25,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running port: ${PORT} `);
